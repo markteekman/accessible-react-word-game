86c35734f425a9eb3720845c3886a7276b6d7ac7
Mark Teekman <markteekman@users.noreply.github.com>
2023-08-14 20:51:37 +0200

    Update GuessInput to receive and render multiple Guess instances

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Guess from '../Guess/Guess';
+
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess) => (
-        <p
-          key={guess}
-          className="guess"
-        >
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess
+          key={num}
+          word={guesses[num]}
+        />
       ))}
     </div>
   );
