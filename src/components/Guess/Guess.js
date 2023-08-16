5530d0ecacc858d72636c4c6402c5c4307591814
Mark Teekman <markteekman@users.noreply.github.com>
2023-08-16 20:27:12 +0200

    Refactor components and use checkGuess to apply classes/hints to words

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ word }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ word, answer }) {
+  const result = checkGuess(word, answer);
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span
+        <Cell
           key={num}
-          className="cell"
-        >
-          {word ? word[num] : undefined}
-        </span>
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
       ))}
     </p>
   );
